0b02f3c2814e0bdeadfdcd1ba7c7e092d5f73b47
Mark Joseph Alconcel <109438699+MJAlconcel8@users.noreply.github.com>
2022-11-05 17:57:08 -0400

    calendar update
    added accurate day and month

--- app/src/main/java/com/example/reminderapp/Calendar.java
+++ app/src/main/java/com/example/reminderapp/Calendar.java
@@ -1,14 +1,27 @@
 package com.example.reminderapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class Calendar extends AppCompatActivity {
-
+    CalendarView calendarView;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                String date = i2+"/"+i1+"/";
+                textView.setText(date);
+
+            }
+        });
     }
 }
